38fd3dac9e544a7cf94fad6a95e6b9b1c1ca14d0
Justina Stupenkaite <st.justina.st@gmail.com>
2012-10-02 16:46:08 +0200

    papildziau veiksmai

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 int main (int argc, char const *argv[])
 {
+	int a = 10, b = 5, suma, skirtumas, daugyba, dalyba, liekana, sveikadalis, laipsnis;
+        suma = a + b;
+        skirtumas = a - b;
+        daugyba = a * b;
+        dalyba = a / 10;
+        liekana a % b;
+        sveikadalis = a / b;
+        laipsnis = a^b;
 	cout << "Sveika aritmetika!";
 	return 0;
 }
